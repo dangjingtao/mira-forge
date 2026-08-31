0053728479037899cff77896df6a20fc2f899eee
DangJingtao <dangjingtao@163.com>
2026-08-31 14:09:10 +0800

    T017 integrate main thread into Forge shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import FirstRunCheck from './FirstRunCheck'
 import MainThreadPanel from './MainThreadPanel'
 import ShortcutFeedback from './ShortcutFeedback'
 import './styles.css'
@@ -8,8 +9,15 @@ import './styles.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <>
-      <App />
-      <MainThreadPanel />
+      <div className="forge-product-shell">
+        <App />
+        <aside className="forge-thread-rail" aria-label="Main thread workspace">
+          <MainThreadPanel />
+          <div className="forge-rail-footer">
+            <FirstRunCheck />
+          </div>
+        </aside>
+      </div>
       <ShortcutFeedback />
     </>
   </StrictMode>,
