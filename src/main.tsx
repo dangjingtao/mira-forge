e8d35d75c8823553120d3a74e189e0a1672f53fd
DangJingtao <dangjingtao@163.com>
2026-08-31 14:12:18 +0800

    T017 preserve current first-run entry visibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import FirstRunCheck from './FirstRunCheck'
 import MainThreadPanel from './MainThreadPanel'
 import ShortcutFeedback from './ShortcutFeedback'
 import './styles.css'
@@ -13,9 +12,6 @@ createRoot(document.getElementById('root')!).render(
         <App />
         <aside className="forge-thread-rail" aria-label="Main thread workspace">
           <MainThreadPanel />
-          <div className="forge-rail-footer">
-            <FirstRunCheck />
-          </div>
         </aside>
       </div>
       <ShortcutFeedback />
